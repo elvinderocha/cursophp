6cf1ddf15361892e92d3724c5b967adf93425a68
elvinderocha <elvin@engage.com.br>
2017-12-13 15:38:15 -0200

    teste
    teste

---
+++ 03-post.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Aula 03 - Curso de PHP</title>
+	</head>
+	<body>
+		
+
+		<FORM id="frmRender" action="http://reports.engbox.com.br/dashboard-ws?/engBOX_Reports/STG_Report_eCampaign_ActionEmailResults" method="post" target="_self">  
+   <INPUT type="hidden" name="rs:Command" value="Render">   
+   <INPUT type="hidden" name="rc:LinkTarget" value="main">  
+   <INPUT type="hidden" name="rs:Format" value="HTML4.0">  
+   <INPUT type="hidden" name="AcaoEmail" value="484">  
+   <INPUT type="submit" value="Button">  
+</FORM> 
+		
+	</body>
+</html>
